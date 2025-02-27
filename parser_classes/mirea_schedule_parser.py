070416eec20091103927f330e357e4f8dcb11201
Владимир <baranov-v-k@yandex.ru>
2025-02-27 12:41:39 +0300

    Создан процесс для загрузки расписания по названию группы

--- parser_classes/mirea_schedule_parser.py
+++ parser_classes/mirea_schedule_parser.py
@@ -3,6 +3,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")  # Включаем headless-режим
@@ -13,20 +15,20 @@ driver = webdriver.Chrome(options=options)
 
 # Открываем страницу
 driver.get("https://www.mirea.ru/schedule/")
-
-# ---
+original_size = driver.get_window_size()
 
 # Ждём загрузки страницы
 # time.sleep(3)
 
 # Находим iframe (например, по ID)
-iframe = driver.find_element(By.ID, 'schedule_iframe')  # Замените 'iframe-id' на реальный ID
+iframe = driver.find_element(By.ID, 'schedule_iframe')
 
 # Переключаемся на iframe
 driver.switch_to.frame(iframe)
 
 # Теперь ищем элемент внутри iframe
 element = driver.find_element(By.ID, 'rs-:Rlhr6:')
+
 
 # Выводим информацию об элементе
 # print("Текст элемента:", element.text)
@@ -37,17 +39,27 @@ element = driver.find_element(By.ID, 'rs-:Rlhr6:')
 element.send_keys('УДМО-01-24')
 
 # Ждём, чтобы увидеть результат
-# time.sleep(3)
+time.sleep(3)
+
+element.send_keys(Keys.ARROW_DOWN)  # Navigate down to the desired suggestion
+element.send_keys(Keys.ENTER)
+
+time.sleep(3)
+
 
 # Делаем скриншот, чтобы сохранить результат
-driver.save_screenshot('screenshot.png')
+# driver.save_screenshot('screenshot.png')
 
 # (Опционально) Возвращаемся на основную страницу
 driver.switch_to.default_content()
 
+required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+driver.set_window_size(required_width, required_height)
+driver.save_screenshot("screenshot.png")  # has scrollbar
+driver.set_window_size(original_size['width'], original_size['height'])
+
 # ---
-
-
 
 # Завершаем работу
 driver.quit()
