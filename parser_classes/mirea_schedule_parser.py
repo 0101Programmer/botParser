48bfcd0483f9a886dbfb0fec1d3ef89355a584f9
Владимир <baranov-v-k@yandex.ru>
2025-02-27 14:58:32 +0300

    Создан класс для парсинга расписания с сайта МИРЭА

--- parser_classes/mirea_schedule_parser.py
+++ parser_classes/mirea_schedule_parser.py
@@ -2,98 +2,91 @@ import time
 from PIL import Image
 
 from selenium import webdriver
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
-from pathlib import Path
+class MireaScheduleParser:
+    def __init__(self):
+        # url сайта
+        self.mirea_schedule_url = "https://www.mirea.ru/schedule/"
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument("--headless")  # Включаем headless-режим
+        self.options.add_argument("--disable-gpu")  # Отключаем GPU (рекомендуется для headless)
+        # Создаем объект браузера (в данном случае Chrome)
+        self.driver = webdriver.Chrome(options=self.options)
+        # заготовка для получения координат рамки с расписанием
+        self.schedule_box_location = ''
 
-# Путь к папке
-folder_path = Path("../media/mirea_schedule_parser_media")
+    def page_parser(self, group_number: str):
+        # Открываем страницу
+        self.driver.get("https://www.mirea.ru/schedule/")
+        # Сохраняем её размер (чтобы потом сделать скриншот)
+        original_size = self.driver.get_window_size()
 
-# Проверка, существует ли папка
-if folder_path.exists() and folder_path.is_dir():
-    print("Папка существует.")
-else:
-    print("Папка не существует.")
+        # Находим iframe с расписанием (+ ждём загрузку)
+        try:
+            iframe = WebDriverWait(self.driver, 10).until(
+                ec.presence_of_element_located((By.ID, "schedule_iframe")))
+            # получаем его координаты, чтобы потом обрезать скриншот всей страницы по ним
+            schedule_box_location = iframe.location
+            self.schedule_box_location = schedule_box_location
+            # Переключаемся на iframe
+            self.driver.switch_to.frame(iframe)
+            try:
+                # Теперь ищем элемент внутри iframe
+                element = WebDriverWait(self.driver, 10).until(
+                    ec.presence_of_element_located((By.ID, "rs-:Rlhr6:")))
+                # отправляем значение номера группы в поле для ввода
+                element.send_keys(str(group_number))
+                # Ждём загрузки подсказки для выбора номера группы
+                self.driver.implicitly_wait(5)
+                # выбираем предложенный вариант из списка
+                element.send_keys(Keys.ARROW_DOWN)
+                element.send_keys(Keys.ENTER)
+                # Ждём, чтобы увидеть результат
+                self.driver.implicitly_wait(5)
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")  # Включаем headless-режим
-options.add_argument("--disable-gpu")  # Отключаем GPU (рекомендуется для headless)
+            except TimeoutException as e:
+                print(f"Элемент не найден: {e}")
 
-# Создаем объект браузера (в данном случае Chrome)
-driver = webdriver.Chrome(options=options)
+        except TimeoutException as e:
+            print(f"Элемент не найден: {e}")
+        finally:
 
-# Открываем страницу
-driver.get("https://www.mirea.ru/schedule/")
-original_size = driver.get_window_size()
+            # Возвращаемся на основную страницу
+            self.driver.switch_to.default_content()
 
-# Находим iframe (например, по ID)
-iframe = driver.find_element(By.ID, 'schedule_iframe')
+            # делаем скриншот страницы (по размеру страницы)
+            required_width = self.driver.execute_script('return document.body.parentNode.scrollWidth')
+            required_height = self.driver.execute_script('return document.body.parentNode.scrollHeight')
+            self.driver.set_window_size(required_width, required_height)
+            self.driver.save_screenshot("screenshot.png")
+            self.driver.set_window_size(original_size['width'], original_size['height'])
 
+            # Загружаем скриншот с помощью Pillow
+            screenshot = Image.open("screenshot.png")
 
-location = iframe.location
-print(location)
+            # Координаты области, которую нужно вырезать
+            left = self.schedule_box_location['x']  # x-координата
+            top = self.schedule_box_location['y']  # y-координата
+            width = 500  # ширина области
+            height = 750  # высота области
 
+            # Вычисляем правую и нижнюю границы
+            right = left + width + 80
+            bottom = top + height + 150
 
-# Переключаемся на iframe
-driver.switch_to.frame(iframe)
+            # Обрезаем скриншот до нужной области
+            cropped_screenshot = screenshot.crop((left, top, right, bottom))
 
-# Теперь ищем элемент внутри iframe
-element = driver.find_element(By.ID, 'rs-:Rlhr6:')
+            # Сохраняем обрезанный скриншот
+            cropped_screenshot.save("../media/mirea_schedule_parser_media/cropped_screenshot.png")
 
+            self.driver.quit()
 
-# Выводим информацию об элементе
-# print("Текст элемента:", element.text)
-# print("ID элемента:", element.get_attribute('id'))
-# print("Класс элемента:", element.get_attribute('class'))
-# print("HTML-код элемента:", element.get_attribute('outerHTML'))
+test_class = MireaScheduleParser()
 
-element.send_keys('УДМО-01-24')
-
-# Ждём, чтобы увидеть результат
-time.sleep(3)
-
-element.send_keys(Keys.ARROW_DOWN)  # Navigate down to the desired suggestion
-element.send_keys(Keys.ENTER)
-
-time.sleep(3)
-
-
-# Делаем скриншот, чтобы сохранить результат
-# driver.save_screenshot('screenshot.png')
-
-# (Опционально) Возвращаемся на основную страницу
-driver.switch_to.default_content()
-
-required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
-required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
-driver.set_window_size(required_width, required_height)
-driver.save_screenshot("screenshot.png")
-driver.set_window_size(original_size['width'], original_size['height'])
-
-# ---
-
-# Загружаем скриншот с помощью Pillow
-screenshot = Image.open("screenshot.png")
-
-# Координаты области, которую нужно вырезать
-left = 40    # x-координата
-top = 773    # y-координата
-width = 500  # ширина области (укажите нужное значение)
-height = 750 # высота области (укажите нужное значение)
-
-# Вычисляем правую и нижнюю границы
-right = left + width + 80
-bottom = top + height + 150
-
-# Обрезаем скриншот до нужной области
-cropped_screenshot = screenshot.crop((left, top, right, bottom))
-
-# Сохраняем обрезанный скриншот
-cropped_screenshot.save("../media/mirea_schedule_parser_media/cropped_screenshot.png")
-
-
-# Завершаем работу
-driver.quit()
+test_class.page_parser("УДМО-01-24")
